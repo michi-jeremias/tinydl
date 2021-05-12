04e4be8759bf18077bcb5e34416d9b99eb8d8b9a
michael.jeremias <michael.jeremias.home@gmail.com>
2021-05-12 14:59:31 +0200

    DCGAN for MNIST

--- DCGAN-1-MNIST-pt.py
+++ DCGAN-1-MNIST-pt.py
@@ -1,4 +1,5 @@
-"""09-05-2021: DCGAN implementation."""
+"""09-05-2021: DCGAN implementation,
+see https://arxiv.org/abs/1511.06434 for the paper."""
 
 # Imports
 import torch
@@ -20,38 +21,51 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # Hyperparameters
 DATAPATH = '../data'
 TBLOGPATH = '../logs/tensorboard/DCGAN/MNIST'
-batch_size = 64
-num_epochs = 20
-learning_rate = 3e-4
-z_dim = 64
-img_channels = 3
+batch_size = 128
+num_epochs = 5
+learning_rate = 2e-4
+beta1 = 0.5
+beta2 = 0.999
+z_dim = 100
+img_channels = 1
 num_features = 8
+IMG_SIZE = 64  # MNIST
 
 
 # Models
 def init_weights(layer):
-    if isinstance(layer, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
-        nn.init.normal_(layer.weight, 0.0, 0.02)
+    if isinstance(layer, (nn.Conv2d, nn.BatchNorm2d, nn.ConvTranspose2d)):
+        nn.init.normal_(tensor=layer.weight, mean=0.0, std=0.02)
 
 
 class Discriminator(nn.Module):
 
-    def __init__(self, img_channels, num_features):
+    def __init__(self, img_channels):
         super().__init__()
         self.img_channels = img_channels
-        self.num_features = num_features
-        # Input shape is [batch_size, 3, 64, 64]
-        # Output shape is [batch_size, 1]
         self.net = nn.Sequential(
-            nn.Conv2d(
-                in_channels=self.img_channels,
-                out_channels=self.num_features,
-                kernel_size=4, stride=2, padding=1),
+            # 64 to 32
+            nn.Conv2d(in_channels=self.img_channels, out_channels=128,
+                      kernel_size=4, stride=2, padding=1, bias=False),
             nn.LeakyReLU(0.2),
-            self._block(num_features, num_features * 2, 4, 2, 1),
-            self._block(num_features * 2, num_features * 4, 4, 2, 1),
-            self._block(num_features * 4, num_features * 8, 4, 2, 1),
-            self._block(num_features * 8, 1, 4, 2, 0),
+            # 32 to 16
+            nn.Conv2d(in_channels=128, out_channels=256,
+                      kernel_size=4, stride=2, padding=1, bias=False),
+            nn.LeakyReLU(0.2),
+            nn.BatchNorm2d(num_features=256),
+            # 16 to 8
+            nn.Conv2d(in_channels=256, out_channels=512,
+                      kernel_size=4, stride=2, padding=1, bias=False),
+            nn.LeakyReLU(0.2),
+            nn.BatchNorm2d(num_features=512),
+            # 8 to 4
+            nn.Conv2d(in_channels=512, out_channels=1024,
+                      kernel_size=4, stride=2, padding=1, bias=False),
+            nn.LeakyReLU(0.2),
+            nn.BatchNorm2d(1024),
+            # 4 to 1
+            nn.Conv2d(in_channels=1024, out_channels=1,
+                      kernel_size=4, stride=2, padding=0),
             nn.Sigmoid()
         )
         self.net.apply(init_weights)
@@ -59,30 +73,32 @@ class Discriminator(nn.Module):
     def forward(self, x):
         return self.net(x)
 
-    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
-        return nn.Sequential(
-            nn.Conv2d(in_channels, out_channels,
-                      kernel_size, stride, padding, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.LeakyReLU(0.2)
-        )
-
 
 class Generator(nn.Module):
-
-    def __init__(self, z_dim, img_channels, num_features):
+    def __init__(self, z_dim, img_channels):
         super().__init__()
-        self.z_dim = z_dim
         self.img_channels = img_channels
-        self.num_features = num_features
-        # Input shape: [batch_size, z_dim]
-        # Output: [batch_size, num_channels=3, 64, 64]
+        self.z_dim = z_dim
+        # n_out = (n_in - 1)*stride-2*pad_in + kernel_size + pad_out
         self.net = nn.Sequential(
-            self._block(z_dim, num_features * 16, 4, 1, 0),
-            self._block(num_features * 16, num_features * 8, 4, 2, 1),
-            self._block(num_features * 8, num_features * 4, 4, 2, 1),
-            self._block(num_features * 4, num_features * 2, 4, 2, 1),
-            nn.ConvTranspose2d(num_features * 2, self.img_channels, 4, 2, 1),
+            nn.ConvTranspose2d(in_channels=self.z_dim, out_channels=1024,
+                               kernel_size=4, stride=1, padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_features=1024),
+            nn.ConvTranspose2d(in_channels=1024, out_channels=512,
+                               kernel_size=4, stride=2, padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_features=512),
+            nn.ConvTranspose2d(in_channels=512, out_channels=256,
+                               kernel_size=4, stride=2, padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_features=256),
+            nn.ConvTranspose2d(in_channels=256, out_channels=128,
+                               kernel_size=4, stride=2, padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_features=128),
+            nn.ConvTranspose2d(in_channels=128, out_channels=self.img_channels,
+                               kernel_size=4, stride=2, padding=1),
             nn.Tanh()
         )
         self.net.apply(init_weights)
@@ -90,35 +106,29 @@ class Generator(nn.Module):
     def forward(self, x):
         return self.net(x)
 
-    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
-        return nn.Sequential(
-            nn.ConvTranspose2d(in_channels, out_channels,
-                               kernel_size, stride, padding, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU()
-        )
 
+D = Discriminator(img_channels=1).to(device=device)
+G = Generator(z_dim=z_dim, img_channels=1).to(device=device)
 
-D = Discriminator(img_channels, num_features).to(device=device)
-G = Generator(z_dim, img_channels, num_features).to(device=device)
 
 # Test
-
-
 def test():
-    N, in_channels, H, W = 8, 3, 64, 64
+    N, img_channels, H, W = 8, 3, 64, 64
     z_dim = 100
-    z = torch.randn(N, in_channels, H, W)
-    D = Discriminator(in_channels, 8)
+    z = torch.rand(N, img_channels, H, W)
+    D = Discriminator(img_channels)
     assert (D(z)).shape == (N, 1, 1, 1)
     print('nice')
 
 
 # Data
 transform = transforms.Compose([
-    transforms.ToTensor()
+    transforms.Resize(IMG_SIZE),
+    transforms.ToTensor(),
+    transforms.Normalize([0.5 for _ in range(img_channels)],
+                         [0.5 for _ in range(img_channels)])
 ])
-dataset = datasets.MNIST(root=DATAPATH, tansform=transform, download=True)
+dataset = datasets.MNIST(root=DATAPATH, transform=transform, download=True)
 loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
 
@@ -126,11 +136,14 @@ loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 criterion = nn.BCELoss()
 
 # Optimizer
-optim_D = optim.Adam(params=D.parameters(), lr=learning_rate)
-optim_G = optim.Adam(params=G.parameters(), lr=learning_rate)
+optim_D = optim.Adam(params=D.parameters(),
+                     lr=learning_rate, betas=(beta1, beta2))
+optim_G = optim.Adam(params=G.parameters(),
+                     lr=learning_rate, betas=(beta1, beta2))
 
 
 # Training
+@timefunc
 def train():
     print('Start training.')
     fake_writer = SummaryWriter(log_dir=f'{TBLOGPATH}/fake')
@@ -141,22 +154,23 @@ def train():
 
     for epoch in range(num_epochs):
 
-        for batch_idx, (real_data, _) in loader:
+        for batch_idx, (real_data, _) in enumerate(loader):
             current_batch_size = real_data.shape[0]
-            x = real_data.reshape(current_batch_size, -1).to(device=device)
-            z = torch.randn(current_batch_size, z_dim).to(device)
+            x = real_data.to(device=device)
+            # Random uniform
+            z = torch.rand(current_batch_size, z_dim, 1, 1).to(device)
             fake_data = G(z)
 
             # Discriminator forward
-            real_scores = D(x)
-            fake_scores = D(G(z))
+            real_scores = D(x).reshape(-1)
+            fake_scores = D(G(z)).reshape(-1)
             real_loss_D = criterion(real_scores, torch.ones_like(real_scores))
             fake_loss_D = criterion(fake_scores, torch.zeros_like(fake_scores))
-            loss_D = real_loss_D + fake_loss_D
+            loss_D = (real_loss_D + fake_loss_D) / 2
 
             # Discriminator backward
             optim_D.zero_grad()
-            loss_D.backward()  # retain_graph=True
+            loss_D.backward(retain_graph=True)  # retain_graph=True
             optim_D.step()
 
             # Generator forward
@@ -173,15 +187,19 @@ def train():
                 with torch.no_grad():
                     print(
                         f'Epoch [{epoch + 1}/{num_epochs}]'
-                        f'\tLoss D: {loss_D:.6f}\tLoss G:{loss_G:.6f}')
-                    fake_data = G(z).reshape(-1, 1, 28, 28)
+                        f'\tLoss D: {loss_D:.6f}\tLoss G: {loss_G:.6f}')
+                    fake_data = G(z).reshape(-1, 1, IMG_SIZE, IMG_SIZE)
                     fake_grid = torchvision.utils.make_grid(
-                        fake_data, normalize=True)
+                        fake_data[:32], normalize=True)
                     fake_writer.add_image(
                         'Fake Img', fake_grid, global_step=tb_step)
-                    real_data = real_data.reshape(-1, 1, 28, 28)
+                    real_data = real_data.reshape(-1, 1, IMG_SIZE, IMG_SIZE)
                     real_grid = torchvision.utils.make_grid(
-                        real_data, normalize=True)
+                        real_data[:32], normalize=True)
                     real_writer.add_image(
                         'Real Img', real_grid, global_step=tb_step)
                     tb_step += 1
+    print('Finished training.')
+
+
+train()
