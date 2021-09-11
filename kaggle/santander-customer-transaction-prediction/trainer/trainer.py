f3c02f777dd3b57ef076dd4840f75ff46bd348bc
michael.jeremias <michael.jeremias.home@gmail.com>
2021-09-11 19:02:29 +0200

    Update trainer.py

--- kaggle/santander-customer-transaction-prediction/trainer/trainer.py
+++ kaggle/santander-customer-transaction-prediction/trainer/trainer.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from trainer.const import LOGPATH
+import torch
 from torch.utils.tensorboard import SummaryWriter
 import torch.optim as optim
 # writer = SummaryWriter(log_dir=f'{LOGPATH}/MNIST')
@@ -9,19 +10,47 @@ from model.utils import init_normal, init_xavier
 
 class Trainer():
 
-    def __init__(self, model, optimizer) -> None:
+    def __init__(self, model, optimizer, loader) -> None:
+        self.device = torch.device(
+            "cuda" if torch.cuda.is_available() else "cpu")
         self.epochs_trained = 0
-        self.model = model
+        self.loader = loader
+        self.model = model.to(self.device)
         self.optimizer = optimizer
         self.lr = self.optimizer.param_groups[0]['lr']
         self.weight_decay = self.optimizer.param_groups[0]['weight_decay']
 
     def train(self, num_epochs) -> None:
+        """Trains self.model for num_epochs epochs.
+
+        Parameters
+        ----------
+        num_epochs : int
+        """
+
         for epoch in range(num_epochs):
             print(
                 f"[{self.epochs_trained + epoch + 1}/"
                 f"{self.epochs_trained + num_epochs}]")
+            self.validate()
         self.epochs_trained += num_epochs
+
+    def validate(self) -> None:
+        """Reports a metric on the validation set."""
+        print("Validation statistic: ")
+
+        self.model.eval()
+        saved_preds = []
+        true_labels = []
+
+        with torch.no_grad():
+            for x, y in self.loader.train_loader:
+                x = x.to(self.device)
+                y = y.to(self.device)
+                scores = self.model(x)
+                saved_preds += scores.tolist()
+                true_labels += y.tolist()
+        self.model.train()
 
     def reset(self) -> None:
         self.model.apply(init_normal)
@@ -46,4 +75,12 @@ class Trainer():
 
 
 class TensorboardLogger:
+    pass
+
+
+class ScalarLogger(TensorboardLogger):
+    pass
+
+
+class ImageLogger(TensorboardLogger):
     pass
