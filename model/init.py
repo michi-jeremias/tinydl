b90c3c84d68456f2182288d9885b4cab5af404d6
Michael Jeremias <michael.jeremias.home@gmail.com>
2021-10-15 22:11:37 +0200

    model.init.py fixed seed support

--- model/init.py
+++ model/init.py
@@ -1,16 +1,22 @@
+import torch
 import torch.nn as nn
 
 
-def init_xavier(layer):
+def init_xavier(layer, fixed_seed=False):
     if isinstance(
             layer,
             (nn.Linear, nn.BatchNorm2d, nn.Conv2d, nn.ConvTranspose2d)):
+        if fixed_seed:
+            torch.manual_seed(0)
         nn.init.xavier_uniform_(layer.weight)
         layer.bias.data.fill_(0)
 
 
-def init_normal(layer):
+def init_normal(layer, fixed_seed=False):
     if isinstance(
             layer,
             (nn.Linear, nn.BatchNorm2d, nn.Conv2d, nn.ConvTranspose2d)):
+        if fixed_seed:
+            torch.manual_seed(0)
         nn.init.normal_(layer.weight, 0.0, 0.02)
+        layer.bias.data.fill_(0)
