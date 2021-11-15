dab0cc2c4413c293a467eec8e2592af926d6d860
michael.jeremias <michael.jeremias@medcom.at>
2021-11-15 16:00:13 +0100

    TensorboardScalarReporter, TensorboardHparamReporter

--- reporter.py
+++ reporter.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from torch.utils.tensorboard import SummaryWriter
 
 
 class Reporter(ABC):
@@ -20,11 +21,42 @@ class ConsoleReporter(Reporter):
         print(f"({self.name}) {metric.name}: {metric.value}")
 
 
-class TensorboardReporter(Reporter):
+class TensorboardScalarReporter(Reporter):
 
-    def __init__(self, name=None) -> None:
-        self.name = name if name else "TensorboardReporter"
+    def __init__(self, name=None, log_dir=None) -> None:
+        self.name = name if name else "TensorboardScalarReporter"
+        self.log_dir = log_dir
+        self.writer = SummaryWriter(log_dir=log_dir)
+        self.step = 0
 
     def notify(self, metric, *args):
-        # TODO: implement
-        pass
+        print("notify tb logger")
+        self.writer.add_scalar(
+            metric.name,
+            scalar_value=metric.value,
+            global_step=self.step
+        )
+        self.step += 1
+
+
+class TensorboardHparamReporter(Reporter):
+
+    def __init__(self, name=None, log_dir=None) -> None:
+        self.name = name if name else "TensorboardHparamReporter"
+        self.log_dir = log_dir
+        self.writer = SummaryWriter(log_dir=log_dir)
+        self.step = 0
+
+    def notify(self, metric, hparam: dict, *args):
+        print("notify tb logger")
+        self.writer.add_hparams(
+            # hparam_dict={'learningrate': lr, 'batchsize': bs},
+            hparam_dict=hparam,
+            metric_dict={f"{metric.name}": metric.value)
+
+        self.writer.add_scalar(
+            metric.name,
+            scalar_value=metric.value,
+            global_step=self.step
+        )
+        self.step += 1
