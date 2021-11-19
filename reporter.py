a3c8d9131c276935980d0884d3fe6cd7ff994ba4
michael.jeremias <michael.jeremias@medcom.at>
2021-11-19 11:03:05 +0100

    Update reporter.py

--- reporter.py
+++ reporter.py
@@ -26,7 +26,7 @@ class TensorboardScalarReporter(Reporter):
     def __init__(self, name=None, log_dir=None) -> None:
         self.name = name if name else "TensorboardScalarReporter"
         self.log_dir = log_dir
-        self.writer = SummaryWriter(log_dir=log_dir)
+        self.writer = SummaryWriter(log_dir=self.log_dir)
         self.step = 0
 
     def notify(self, metric, *args):
@@ -40,19 +40,23 @@ class TensorboardScalarReporter(Reporter):
 
 
 class TensorboardHparamReporter(Reporter):
+    """The TensorboardHparamReporter calls
+    tensorboard.SummaryWriter().add_hparams() with the name and value of
+    the metric."""
 
     def __init__(self, name=None, log_dir=None) -> None:
         self.name = name if name else "TensorboardHparamReporter"
         self.log_dir = log_dir
-        self.writer = SummaryWriter(log_dir=log_dir)
+        self.writer = SummaryWriter(log_dir=self.log_dir)
         self.step = 0
 
-    def notify(self, metric, hparam: dict, *args):
+    def notify(self, metric, *args):
         print("notify tb logger")
+        self.writer.add_scalar(f"{metric.name}", metric.value)
         self.writer.add_hparams(
             # hparam_dict={'learningrate': lr, 'batchsize': bs},
-            hparam_dict=hparam,
-            metric_dict={f"{metric.name}": metric.value)
+            hparam_dict={"batchsize": 128, "lr": 2e-3},
+            metric_dict={f"{metric.name}": metric.value})
 
         self.writer.add_scalar(
             metric.name,
