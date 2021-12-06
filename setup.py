aef193ef5bc46d5a30c2049b9446a2afea729135
michael.jeremias <michael.jeremias@medcom.at>
2021-12-06 12:29:19 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='tiny-dl',
+    version='0.1',
+    scripts=[],
+    author="Michael Jeremias",
+    author_email="michael.jeremias.home@gmail.com",
+    description="A Python library for deeplearning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/michi-jeremias/tiny-dl",
+    packages=setuptools.find_packages(),
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: GNU General Public License v3.0",
+         "Operating System :: OS Independent",
+    ],
+)
