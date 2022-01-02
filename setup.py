912c3ee04678e1996b5c0dc51e082734dd6ded25
Michael Jeremias <michael.jeremias.home@gmail.com>
2022-01-02 10:55:17 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # I explain this later on
     download_url='https://github.com/user/reponame/archive/v_01.tar.gz',
     # Keywords that define your package best
-    keywords=['deep', 'learning', 'pytorch'],
+    keywords=['deep', 'learning', 'pytorch', 'training'],
     install_requires=[            # I get to this in a second
         'validators',
         'beautifulsoup4',
@@ -28,26 +28,3 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
 )
-# import setuptools
-
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
-
-
-# setuptools.setup(
-#     name='tinydl',
-#     version='0.1',
-#     scripts=[],
-#     author="Michael Jeremias",
-#     author_email="michael.jeremias.home@gmail.com",
-#     description="A Python library for deeplearning",
-#     long_description=long_description,
-#     long_description_content_type="text/markdown",
-#     url="https://github.com/michi-jeremias/tinydl",
-#     packages=setuptools.find_packages(),
-#     classifiers=[
-#          "Programming Language :: Python :: 3",
-#          "License :: OSI Approved :: GNU General Public License v3.0",
-#          "Operating System :: OS Independent",
-#     ],
-# )
