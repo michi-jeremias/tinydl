57dcc7550511d8f3d0ead5fdbbbe2d8764a8bf71
Michael Jeremias <michael.jeremias.home@gmail.com>
2022-01-03 10:04:19 +0100

    Update __init__.py

--- tinydl/__init__.py
+++ tinydl/__init__.py
@@ -1,5 +1,5 @@
-from hyperparameter import Hyperparameter
-from metric import BinaryCrossentropy, RocAuc
-from modelinit import init_normal, init_xavier
-from reporter import ConsoleReporter, TensorboardHparamReporter
-from runner import Runner, Trainer, Validator
+from tinydl.hyperparameter import Hyperparameter
+from tinydl.metric import BinaryCrossentropy, RocAuc
+from tinydl.modelinit import init_normal, init_xavier
+from tinydl.reporter import ConsoleReporter, TensorboardHparamReporter
+from tinydl.runner import Runner, Trainer, Validator
