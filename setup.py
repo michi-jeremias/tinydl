fbc25c572584116bff8896c0ee476fad852fc6ef
michael.jeremias <michael.jeremias.home@gmail.com>
2022-01-04 09:22:17 +0100

    install_requires tensorboard

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ setup(
     install_requires=[            # I get to this in a second
         'torch',
         'sklearn',
-        'tqdm'
+        'tqdm',
+        'tensorboard'
     ],
     classifiers=[
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
