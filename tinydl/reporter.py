8bf5f7e2e2147940750e97d46cd7421ec64019e6
michael.jeremias <michael.jeremias.home@gmail.com>
2022-01-08 07:03:46 +0100

    Update reporter.py

--- tinydl/reporter.py
+++ tinydl/reporter.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
+
 from torch.utils.tensorboard import SummaryWriter
-from tinydl import Metric
+
+from tinydl.metric import Metric
 
 
 class Reporter(ABC):
@@ -22,21 +24,21 @@ class ConsoleReporter(Reporter):
         print(f"({self.name}) {metric.name}: {metric.value}")
 
 
-class TensorboardScalarReporter(Reporter):
+# class TensorboardScalarReporter(Reporter):
 
-    def __init__(self, name: str = None, log_dir: str = None) -> None:
-        self.name = name if name else "TensorboardScalarReporter"
-        self.log_dir = log_dir
-        self.writer = SummaryWriter(log_dir=self.log_dir)
-        self.step = 0
+#     def __init__(self, name: str = None, log_dir: str = None) -> None:
+#         self.name = name if name else "TensorboardScalarReporter"
+#         self.log_dir = log_dir
+#         self.writer = SummaryWriter(log_dir=self.log_dir)
+#         self.step = 0
 
-    def notify(self, metric, *args):
-        self.writer.add_scalar(
-            metric.name,
-            scalar_value=metric.value,
-            global_step=self.step
-        )
-        self.step += 1
+#     def notify(self, metric, *args):
+#         self.writer.add_scalar(
+#             metric.name,
+#             scalar_value=metric.value,
+#             global_step=self.step
+#         )
+#         self.step += 1
 
 
 class TensorboardHparamReporter(Reporter):
