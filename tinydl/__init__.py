87c01a372f4cd9ef040e46aac04d353ba7222339
michael.jeremias <michael.jeremias.home@gmail.com>
2022-01-09 23:10:23 +0100

    add Metric and Stage

--- tinydl/__init__.py
+++ tinydl/__init__.py
@@ -1,5 +1,6 @@
 from tinydl.hyperparameter import Hyperparameter
-from tinydl.metric import BinaryCrossentropy, RocAuc
+from tinydl.metric import BinaryCrossentropy, Metric, RocAuc
 from tinydl.modelinit import init_normal, init_xavier
 from tinydl.reporter import ConsoleReporter, TensorboardHparamReporter
 from tinydl.runner import Runner, Trainer, Validator
+from tinydl.stage import Stage
