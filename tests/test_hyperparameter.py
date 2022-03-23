e6efd800400e246de459589a459cfe726fe97340
michael.jeremias <michael.jeremias.home@gmail.com>
2022-03-23 10:17:51 +0100

    add new tests

--- tests/test_hyperparameter.py
+++ tests/test_hyperparameter.py
@@ -8,6 +8,15 @@ class TestHyperparameter(unittest.TestCase):
     def test_get_single_experiment(self):
         hparam = {"lr": [1e-3], "bs": [128]}
         hyper = Hyperparameter(hparam)
+        experiments = hyper.get_experiments()
+        experiment_list = []
+        for experiment in experiments:
+            experiment_list.append(experiment)
+
+        expected_result = [
+            {"lr": [1e-3], "bs": [128]}
+        ]
+        self.assertEqual(experiment_list, expected_result)
 
     def test_num_single_experiments(self):
         hparam = {"lr": [1e-3], "bs": [128]}
@@ -28,3 +37,19 @@ class TestHyperparameter(unittest.TestCase):
         hparam = {"lr": [1e-3, 1e-4], "bs": [128, 256]}
         hyper = Hyperparameter(hparam)
         self.assertEqual(hyper.num_experiments, 4)
+
+    def test_four_experiments(self):
+        hparam = {"lr": [1e-3, 1e-4], "bs": [128, 256]}
+        hyper = Hyperparameter(hparam)
+        experiments = hyper.get_experiments()
+        experiment_list = []
+        for experiment in experiments:
+            experiment_list.append(experiment)
+
+        expected_result = [
+            {"lr": [1e-3], "bs": [128]},
+            {"lr": [1e-4], "bs": [128]},
+            {"lr": [1e-3], "bs": [256]},
+            {"lr": [1e-4], "bs": [256]},
+        ]
+        self.assertEqual(experiment_list, expected_result)
