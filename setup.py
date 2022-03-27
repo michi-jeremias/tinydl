eadfd1d2073ba2ea5ef82312cbd123b383279cee
Michael Jeremias <michael.jeremias.home@gmail.com>
2022-03-27 11:16:26 +0200

    v.0.1.2

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from distutils.core import setup
 setup(
     name='tinydl',
     packages=['tinydl'],
-    version='0.1.1',
+    version='0.1.2',
     license='MIT',
     description='Python library which facilitates training and validation of deep learning models implemented with PyTorch.',
     author='Michael Jeremias',
     author_email='michael.jeremias.home@gmail.com',
     url='https://github.com/michi-jeremias/tinydl',
     # I explain this later on
-    download_url='https://github.com/michi-jeremias/tinydl/archive/refs/tags/v0.1.1.tar.gz',
+    download_url='https://github.com/michi-jeremias/tinydl/archive/refs/tags/v0.1.2.tar.gz',
     # Keywords that define your package best
     keywords=['deep', 'learning', 'pytorch', 'training'],
-    install_requires=[            # I get to this in a second
+    install_requires=[
         'torch',
         'sklearn',
         'tqdm',
@@ -28,5 +28,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
