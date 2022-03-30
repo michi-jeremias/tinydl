956ba323452a030a7915c61db0bb21da793d0fdf
michael.jeremias <michael.jeremias.home@gmail.com>
2022-03-30 13:05:13 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name='tinydl',
     packages=['tinydl'],
-    version='0.1.2',
+    version='0.1.3',
     license='MIT',
     description='Python library which facilitates training and validation of deep learning models implemented with PyTorch.',
     author='Michael Jeremias',
@@ -29,6 +29,5 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
     ],
 )
