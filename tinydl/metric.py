ea9a23fcb23c64ee9c309f9809efd971e5adf451
michael.jeremias <michael.jeremias.home@gmail.com>
2022-03-31 12:44:36 +0200

    added typehints

--- tinydl/metric.py
+++ tinydl/metric.py
@@ -9,7 +9,8 @@ class Metric(ABC):
     """Interface for a Metric. The metric gets reported through a
     Reporter."""
 
-    def __init__(self, name: str = None) -> None:
+    def __init__(self,
+                 name: str = None) -> None:
         self.name = name
 
     def __repr__(self) -> str:
@@ -24,20 +25,24 @@ class Metric(ABC):
 
 class BinaryCrossentropy(Metric):
 
-    def __init__(self, name: str = None) -> None:
+    def __init__(self,
+                 name: str = None) -> None:
         super().__init__()
         self.name = "BCE" if not name else name
         self.value = -1.
         self.bce_loss = BCELoss()
 
-    def calculate(self, scores, targets) -> Tensor:
+    def calculate(self,
+                  scores: Tensor,
+                  targets: Tensor) -> Tensor:
         self.value = self.bce_loss(scores, targets)
         return self.value
 
 
 class DummyMetric(Metric):
 
-    def __init__(self, name: str = None) -> None:
+    def __init__(self,
+                 name: str = None) -> None:
         super().__init__()
         self.name = "Dummy Metric" if not name else name
         self.value = 1.
@@ -49,11 +54,14 @@ class DummyMetric(Metric):
 
 class RocAuc(Metric):
 
-    def __init__(self, name: str = None) -> None:
+    def __init__(self,
+                 name: str = None) -> None:
         super().__init__()
         self.name = "ROC_AUC" if not name else name
         self.value = -1.
 
-    def calculate(self, scores, targets) -> None:
+    def calculate(self,
+                  scores: Tensor,
+                  targets: Tensor) -> None:
         self.value = sklearn.metrics.roc_auc_score(targets, scores)
         return self.value
